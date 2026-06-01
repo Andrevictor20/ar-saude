3fe8b49d8c5c194a046afa049dd046f2260341c7
André Victor Macedo Pereira <victorandre280@gmail.com>
2026-06-01 00:20:28 -0300

    fix(backend, frontend): auto-update neighborhood coords via nominatim api and regen map

--- src/common/constants/neighborhoods.ts
+++ src/common/constants/neighborhoods.ts
@@ -6,91 +6,91 @@ export interface Neighborhood {
 }
 
 export const SAO_LUIS_NEIGHBORHOODS: Neighborhood[] = [
-  { id: 'alemanha', name: 'Alemanha', latitude: -2.5480, longitude: -44.2820 },
-  { id: 'alto_da_esperanca', name: 'Alto da Esperança', latitude: -2.5930, longitude: -44.3370 },
-  { id: 'angelim', name: 'Angelim', latitude: -2.5342, longitude: -44.2305 },
-  { id: 'anil', name: 'Anil', latitude: -2.5358, longitude: -44.2464 },
-  { id: 'anjo_da_guarda', name: 'Anjo da Guarda', latitude: -2.5855, longitude: -44.3312 },
-  { id: 'apeadouro', name: 'Apeadouro', latitude: -2.5455, longitude: -44.2870 },
-  { id: 'apicum', name: 'Apicum', latitude: -2.5250, longitude: -44.2990 },
-  { id: 'areinha', name: 'Areinha', latitude: -2.5570, longitude: -44.2950 },
-  { id: 'vila_bacanga', name: 'Vila Bacanga', latitude: -2.5740, longitude: -44.3200 },
+  { id: 'alemanha', name: 'Alemanha', latitude: -2.5360, longitude: -44.2688 },
+  { id: 'alto_da_esperanca', name: 'Alto da Esperança', latitude: -2.5480, longitude: -44.3148 },
+  { id: 'angelim', name: 'Angelim', latitude: -2.5304, longitude: -44.2353 },
+  { id: 'anil', name: 'Anil', latitude: -2.5467, longitude: -44.2386 },
+  { id: 'anjo_da_guarda', name: 'Anjo da Guarda', latitude: -2.5601, longitude: -44.3311 },
+  { id: 'apeadouro', name: 'Apeadouro', latitude: -2.5398, longitude: -44.2775 },
+  { id: 'apicum', name: 'Apicum', latitude: -2.5344, longitude: -44.2944 },
+  { id: 'areinha', name: 'Areinha', latitude: -2.5457, longitude: -44.2926 },
+  { id: 'vila_bacanga', name: 'Vila Bacanga', latitude: -2.5565, longitude: -44.3139 },
 
-  { id: 'bairro_de_fatima', name: 'Bairro de Fátima', latitude: -2.5513, longitude: -44.2796 },
-  { id: 'bequimao', name: 'Bequimão', latitude: -2.5250, longitude: -44.2611 },
-  { id: 'boa_vista', name: 'Boa Vista', latitude: -2.5700, longitude: -44.2900 },
-  { id: 'bom_milagre', name: 'Bom Milagre', latitude: -2.5460, longitude: -44.2740 },
+  { id: 'bairro_de_fatima', name: 'Bairro de Fátima', latitude: -2.5459, longitude: -44.2831 },
+  { id: 'bequimao', name: 'Bequimão', latitude: -2.5294, longitude: -44.2461 },
+  { id: 'boa_vista', name: 'Boa Vista', latitude: -2.5743, longitude: -44.2945 },
+  { id: 'bom_milagre', name: 'Bom Milagre', latitude: -2.5398, longitude: -44.2816 },
 
-  { id: 'calhau', name: 'Calhau', latitude: -2.4883, longitude: -44.2758 },
-  { id: 'camboa', name: 'Camboa', latitude: -2.5350, longitude: -44.3110 },
-  { id: 'canto_da_fabril', name: 'Canto da Fabril', latitude: -2.5280, longitude: -44.3080 },
-  { id: 'centro', name: 'Centro', latitude: -2.5283, longitude: -44.3044 },
+  { id: 'calhau', name: 'Calhau', latitude: -2.4909, longitude: -44.2692 },
+  { id: 'camboa', name: 'Camboa', latitude: -2.5255, longitude: -44.2901 },
+  { id: 'canto_da_fabril', name: 'Canto da Fabril', latitude: -2.5338, longitude: -44.2896 },
+  { id: 'centro', name: 'Centro', latitude: -2.5318, longitude: -44.2991 },
 
-  { id: 'cidade_olimpica', name: 'Cidade Olímpica', latitude: -2.6030, longitude: -44.2250 },
-  { id: 'cidade_operaria', name: 'Cidade Operária', latitude: -2.5833, longitude: -44.2167 },
+  { id: 'cidade_olimpica', name: 'Cidade Olímpica', latitude: -2.5943, longitude: -44.1835 },
+  { id: 'cidade_operaria', name: 'Cidade Operária', latitude: -2.5762, longitude: -44.1978 },
 
-  { id: 'cohab', name: 'Cohab', latitude: -2.5500, longitude: -44.2200 },
+  { id: 'cohab', name: 'Cohab', latitude: -2.5412, longitude: -44.2189 },
   { id: 'cohab_var1', name: 'Cohab (Variante 1)', latitude: -2.5480, longitude: -44.2230 },
 
-  { id: 'cohafuma', name: 'Cohafuma', latitude: -2.5120, longitude: -44.2600 },
-  { id: 'cohama', name: 'Cohama', latitude: -2.5188, longitude: -44.2541 },
+  { id: 'cohafuma', name: 'Cohafuma', latitude: -2.5078, longitude: -44.2668 },
+  { id: 'cohama', name: 'Cohama', latitude: -2.5141, longitude: -44.2459 },
 
-  { id: 'cohatrac_1', name: 'Cohatrac 1', latitude: -2.5408, longitude: -44.2055 },
-  { id: 'cohatrac_2', name: 'Cohatrac 2', latitude: -2.5450, longitude: -44.2080 },
-  { id: 'cohatrac_3', name: 'Cohatrac 3', latitude: -2.5500, longitude: -44.2100 },
-  { id: 'cohatrac_4', name: 'Cohatrac 4', latitude: -2.5530, longitude: -44.2120 },
-  { id: 'cohatrac_5', name: 'Cohatrac 5', latitude: -2.5451, longitude: -44.2076 },
+  { id: 'cohatrac_1', name: 'Cohatrac 1', latitude: -2.5434, longitude: -44.2091 },
+  { id: 'cohatrac_2', name: 'Cohatrac 2', latitude: -2.5376, longitude: -44.2116 },
+  { id: 'cohatrac_3', name: 'Cohatrac 3', latitude: -2.5375, longitude: -44.2136 },
+  { id: 'cohatrac_4', name: 'Cohatrac 4', latitude: -2.5430, longitude: -44.2090 },
+  { id: 'cohatrac_5', name: 'Cohatrac 5', latitude: -2.5375, longitude: -44.2142 },
 
   { id: 'coheb_filipinho', name: 'Coheb Filipinho', latitude: -2.5500, longitude: -44.2700 },
 
-  { id: 'coroadinho', name: 'Coroadinho', latitude: -2.5647, longitude: -44.2694 },
-  { id: 'coroado', name: 'Coroado', latitude: -2.5530, longitude: -44.2650 },
+  { id: 'coroadinho', name: 'Coroadinho', latitude: -2.5630, longitude: -44.2641 },
+  { id: 'coroado', name: 'Coroado', latitude: -2.5532, longitude: -44.2703 },
 
-  { id: 'diamante', name: 'Diamante', latitude: -2.5600, longitude: -44.2870 },
-  { id: 'divineia', name: 'Divinéia', latitude: -2.5060, longitude: -44.2740 },
+  { id: 'diamante', name: 'Diamante', latitude: -2.5305, longitude: -44.2900 },
+  { id: 'divineia', name: 'Divinéia', latitude: -2.4936, longitude: -44.2188 },
   { id: 'estiva', name: 'Estiva', latitude: -2.7570, longitude: -44.3515 },
 
-  { id: 'filipinho', name: 'Filipinho', latitude: -2.5480, longitude: -44.2720 },
-  { id: 'forquilha', name: 'Forquilha', latitude: -2.5720, longitude: -44.2390 },
-  { id: 'fumaca', name: 'Fumacê', latitude: -2.5620, longitude: -44.2810 },
+  { id: 'filipinho', name: 'Filipinho', latitude: -2.5543, longitude: -44.2639 },
+  { id: 'forquilha', name: 'Forquilha', latitude: -2.5533, longitude: -44.2158 },
+  { id: 'fumaca', name: 'Fumacê', latitude: -2.5611, longitude: -44.3375 },
 
-  { id: 'gancharia', name: 'Gancharia', latitude: -2.5310, longitude: -44.3120 },
+  { id: 'gancharia', name: 'Gancharia', latitude: -2.5549, longitude: -44.3213 },
   { id: 'gapara', name: 'Gapara', latitude: -2.5894, longitude: -44.3003 },
-  { id: 'geniparana', name: 'Geniparana', latitude: -2.6500, longitude: -44.2600 },
+  { id: 'geniparana', name: 'Geniparana', latitude: -2.5815, longitude: -44.1856 },
 
-  { id: 'ilha_da_paz', name: 'Ilha da Paz', latitude: -2.5900, longitude: -44.3200 },
+  { id: 'ilha_da_paz', name: 'Ilha da Paz', latitude: -2.5379, longitude: -44.3341 },
 
-  { id: 'ipase', name: 'Ipase', latitude: -2.5200, longitude: -44.2480 },
-  { id: 'ipem', name: 'Ipem', latitude: -2.5120, longitude: -44.2470 },
-  { id: 'ipem_turu', name: 'Ipem Turu', latitude: -2.5070, longitude: -44.2290 },
+  { id: 'ipase', name: 'Ipase', latitude: -2.5336, longitude: -44.2599 },
+  { id: 'ipem', name: 'Ipem', latitude: -2.5685, longitude: -44.2195 },
+  { id: 'ipem_turu', name: 'Ipem Turu', latitude: -2.5218, longitude: -44.2169 },
 
-  { id: 'itaqui_bacanga', name: 'Itaqui-Bacanga', latitude: -2.5714, longitude: -44.3188 },
-  { id: 'ivar_saldanha', name: 'Ivar Saldanha', latitude: -2.5600, longitude: -44.2860 },
+  { id: 'itaqui_bacanga', name: 'Itaqui-Bacanga', latitude: -2.5515, longitude: -44.3067 },
+  { id: 'ivar_saldanha', name: 'Ivar Saldanha', latitude: -2.5441, longitude: -44.2732 },
 
-  { id: 'janaina', name: 'Janaina', latitude: -2.5790, longitude: -44.2210 },
-  { id: 'jardim_america', name: 'Jardim América', latitude: -2.5900, longitude: -44.2200 },
+  { id: 'janaina', name: 'Janaina', latitude: -2.5906, longitude: -44.1912 },
+  { id: 'jardim_america', name: 'Jardim América', latitude: -2.5851, longitude: -44.1967 },
 
-  { id: 'joao_de_deus', name: 'João de Deus', latitude: -2.5710, longitude: -44.2500 },
-  { id: 'joao_paulo', name: 'João Paulo', latitude: -2.5416, longitude: -44.2713 },
+  { id: 'joao_de_deus', name: 'João de Deus', latitude: -2.5594, longitude: -44.2332 },
+  { id: 'joao_paulo', name: 'João Paulo', latitude: -2.5449, longitude: -44.2724 },
 
   { id: 'jota_lima', name: 'Jota Lima', latitude: -2.5850, longitude: -44.2380 },
-  { id: 'kiola', name: 'Kiola', latitude: -2.6000, longitude: -44.2500 },
+  { id: 'kiola', name: 'Kiola', latitude: -2.5347, longitude: -44.2968 },
 
-  { id: 'liberdade', name: 'Liberdade', latitude: -2.5490, longitude: -44.2950 },
-  { id: 'madre_deus', name: 'Madre Deus', latitude: -2.5340, longitude: -44.2960 },
+  { id: 'liberdade', name: 'Liberdade', latitude: -2.5275, longitude: -44.2845 },
+  { id: 'madre_deus', name: 'Madre Deus', latitude: -2.5406, longitude: -44.2983 },
 
 
   { id: 'maracana', name: 'Maracanã', latitude: -2.6369, longitude: -44.2806 },
-  { id: 'maranhao_novo', name: 'Maranhão Novo', latitude: -2.5255, longitude: -44.2655 },
+  { id: 'maranhao_novo', name: 'Maranhão Novo', latitude: -2.5299, longitude: -44.2541 },
 
-  { id: 'monte_castelo', name: 'Monte Castelo', latitude: -2.5570, longitude: -44.2810 },
+  { id: 'monte_castelo', name: 'Monte Castelo', latitude: -2.5344, longitude: -44.2808 },
 
   { id: 'olho_dagua', name: 'Olho d\'Água', latitude: -2.4842, longitude: -44.2413 },
-  { id: 'outeiro_da_cruz', name: 'Outeiro da Cruz', latitude: -2.5480, longitude: -44.2860 },
+  { id: 'outeiro_da_cruz', name: 'Outeiro da Cruz', latitude: -2.5507, longitude: -44.2554 },
 
-  { id: 'parque_amazonas', name: 'Parque Amazonas', latitude: -2.5780, longitude: -44.2600 },
-  { id: 'parque_timbiras', name: 'Parque Timbiras', latitude: -2.5660, longitude: -44.2480 },
-  { id: 'parque_vitoria', name: 'Parque Vitória', latitude: -2.5300, longitude: -44.2300 },
+  { id: 'parque_amazonas', name: 'Parque Amazonas', latitude: -2.5500, longitude: -44.2870 },
+  { id: 'parque_timbiras', name: 'Parque Timbiras', latitude: -2.5606, longitude: -44.2731 },
+  { id: 'parque_vitoria', name: 'Parque Vitória', latitude: -2.5059, longitude: -44.2113 },
 
 
   { id: 'pedrinhas', name: 'Pedrinhas', latitude: -2.6901, longitude: -44.3051 },
@@ -99,64 +99,64 @@ export const SAO_LUIS_NEIGHBORHOODS: Neighborhood[] = [
 
   { id: 'quebra_pote', name: 'Quebra Pote', latitude: -2.6927, longitude: -44.2172 },
 
-  { id: 'radional', name: 'Radional', latitude: -2.5440, longitude: -44.2680 },
+  { id: 'radional', name: 'Radional', latitude: -2.5467, longitude: -44.2509 },
 
-  { id: 'recanto_dos_vinhais', name: 'Recanto dos Vinhais', latitude: -2.5200, longitude: -44.2660 },
-  { id: 'recanto_fialho', name: 'Recanto Fialho', latitude: -2.5170, longitude: -44.2450 },
+  { id: 'recanto_dos_vinhais', name: 'Recanto dos Vinhais', latitude: -2.5210, longitude: -44.2641 },
+  { id: 'recanto_fialho', name: 'Recanto Fialho', latitude: -2.5090, longitude: -44.2338 },
 
-  { id: 'renascenca', name: 'Renascença', latitude: -2.4999, longitude: -44.2974 },
+  { id: 'renascenca', name: 'Renascença', latitude: -2.5008, longitude: -44.2901 },
 
-  { id: 'residencial_paraiso', name: 'Residencial Paraíso', latitude: -2.6100, longitude: -44.2200 },
-  { id: 'residencial_piramide', name: 'Residencial Pirâmide', latitude: -2.5950, longitude: -44.2150 },
+  { id: 'residencial_paraiso', name: 'Residencial Paraíso', latitude: -2.5727, longitude: -44.2960 },
+  { id: 'residencial_piramide', name: 'Residencial Pirâmide', latitude: -2.4778, longitude: -44.1598 },
 
-  { id: 'rio_grande', name: 'Rio Grande', latitude: -2.6100, longitude: -44.3100 },
-  { id: 'sacavem', name: 'Sacavém', latitude: -2.5450, longitude: -44.2910 },
+  { id: 'rio_grande', name: 'Rio Grande', latitude: -2.6602, longitude: -44.2911 },
+  { id: 'sacavem', name: 'Sacavém', latitude: -2.5555, longitude: -44.2571 },
 
-  { id: 'santa_barbara', name: 'Santa Bárbara', latitude: -2.6200, longitude: -44.2400 },
-  { id: 'santa_clara', name: 'Santa Clara', latitude: -2.5980, longitude: -44.2260 },
-  { id: 'santa_cruz', name: 'Santa Cruz', latitude: -2.6100, longitude: -44.2550 },
+  { id: 'santa_barbara', name: 'Santa Bárbara', latitude: -2.6063, longitude: -44.2127 },
+  { id: 'santa_clara', name: 'Santa Clara', latitude: -2.5967, longitude: -44.1991 },
+  { id: 'santa_cruz', name: 'Santa Cruz', latitude: -2.5464, longitude: -44.2467 },
 
-  { id: 'santo_antonio', name: 'Santo Antônio', latitude: -2.5600, longitude: -44.3000 },
+  { id: 'santo_antonio', name: 'Santo Antônio', latitude: -2.5556, longitude: -44.2468 },
 
-  { id: 'sao_cristovao', name: 'São Cristóvão', latitude: -2.5701, longitude: -44.2258 },
-  { id: 'sao_francisco', name: 'São Francisco', latitude: -2.5036, longitude: -44.3013 },
-  { id: 'sao_raimundo', name: 'São Raimundo', latitude: -2.5900, longitude: -44.3000 },
-  { id: 'sa_viana', name: 'Sa Viana', latitude: -2.5650, longitude: -44.3150 },
+  { id: 'sao_cristovao', name: 'São Cristóvão', latitude: -2.5712, longitude: -44.2345 },
+  { id: 'sao_francisco', name: 'São Francisco', latitude: -2.5119, longitude: -44.3041 },
+  { id: 'sao_raimundo', name: 'São Raimundo', latitude: -2.5967, longitude: -44.2282 },
+  { id: 'sa_viana', name: 'Sa Viana', latitude: -2.5588, longitude: -44.3018 },
 
-  { id: 'sitio_grande', name: 'Sítio Grande', latitude: -2.4700, longitude: -44.2200 },
-  { id: 'sol_e_mar', name: 'Sol e Mar', latitude: -2.4920, longitude: -44.2860 },
+  { id: 'sitio_grande', name: 'Sítio Grande', latitude: -2.5268, longitude: -44.1735 },
+  { id: 'sol_e_mar', name: 'Sol e Mar', latitude: -2.4902, longitude: -44.2156 },
 
-  { id: 'upaon_acu', name: 'Upaon-Açu', latitude: -2.5200, longitude: -44.2500 },
+  { id: 'upaon_acu', name: 'Upaon-Açu', latitude: -2.5094, longitude: -44.2613 },
 
-  { id: 'vassoural', name: 'Vassoural', latitude: -2.5530, longitude: -44.2870 },
-  { id: 'vera_cruz', name: 'Vera Cruz', latitude: -2.6050, longitude: -44.2750 },
+  { id: 'vassoural', name: 'Vassoural', latitude: -2.5065, longitude: -44.1765 },
+  { id: 'vera_cruz', name: 'Vera Cruz', latitude: -2.5461, longitude: -44.2449 },
 
-  { id: 'vicente_fialho', name: 'Vicente Fialho', latitude: -2.5150, longitude: -44.2450 },
+  { id: 'vicente_fialho', name: 'Vicente Fialho', latitude: -2.5085, longitude: -44.2396 },
 
-  { id: 'vila_brasil', name: 'Vila Brasil', latitude: -2.5850, longitude: -44.3100 },
-  { id: 'vila_cascavel', name: 'Vila Cascavel', latitude: -2.5800, longitude: -44.2800 },
+  { id: 'vila_brasil', name: 'Vila Brasil', latitude: -2.5629, longitude: -44.2136 },
+  { id: 'vila_cascavel', name: 'Vila Cascavel', latitude: -2.5943, longitude: -44.2198 },
 
-  { id: 'vila_embratel', name: 'Vila Embratel', latitude: -2.5650, longitude: -44.3250 },
-  { id: 'vila_esperanca', name: 'Vila Esperança', latitude: -2.6100, longitude: -44.2600 },
+  { id: 'vila_embratel', name: 'Vila Embratel', latitude: -2.5671, longitude: -44.3115 },
+  { id: 'vila_esperanca', name: 'Vila Esperança', latitude: -2.6127, longitude: -44.2624 },
 
-  { id: 'vila_flamengo', name: 'Vila Flamengo', latitude: -2.5950, longitude: -44.2450 },
-  { id: 'vila_isabel', name: 'Vila Isabel', latitude: -2.5750, longitude: -44.2850 },
+  { id: 'vila_flamengo', name: 'Vila Flamengo', latitude: -2.5645, longitude: -44.1911 },
+  { id: 'vila_isabel', name: 'Vila Isabel', latitude: -2.5623, longitude: -44.3195 },
 
-  { id: 'vila_lobao', name: 'Vila Lobão', latitude: -2.5700, longitude: -44.2700 },
-  { id: 'vila_luizao', name: 'Vila Luizão', latitude: -2.5000, longitude: -44.2300 },
+  { id: 'vila_lobao', name: 'Vila Lobão', latitude: -2.5642, longitude: -44.2415 },
+  { id: 'vila_luizao', name: 'Vila Luizão', latitude: -2.4924, longitude: -44.2122 },
   { id: 'vila_maranhao', name: 'Vila Maranhão', latitude: -2.6231, longitude: -44.3176 },
 
-  { id: 'vila_nova', name: 'Vila Nova', latitude: -2.6100, longitude: -44.3000 },
-  { id: 'vila_operaria', name: 'Vila Operária', latitude: -2.5650, longitude: -44.2400 },
+  { id: 'vila_nova', name: 'Vila Nova', latitude: -2.5374, longitude: -44.3276 },
+  { id: 'vila_operaria', name: 'Vila Operária', latitude: -2.5670, longitude: -44.1841 },
 
-  { id: 'vila_palmeira', name: 'Vila Palmeira', latitude: -2.5486, longitude: -44.2547 },
+  { id: 'vila_palmeira', name: 'Vila Palmeira', latitude: -2.5419, longitude: -44.2545 },
 
-  { id: 'vila_passos', name: 'Vila Passos', latitude: -2.5350, longitude: -44.2900 },
+  { id: 'vila_passos', name: 'Vila Passos', latitude: -2.5366, longitude: -44.2879 },
 
-  { id: 'vila_riod', name: 'Vila Riod', latitude: -2.6100, longitude: -44.2900 },
+  { id: 'vila_riod', name: 'Vila Riod', latitude: -2.5936, longitude: -44.1951 },
 
   { id: 'vila_sarney', name: 'Vila Sarney', latitude: -2.6302, longitude: -44.2717 },
-  { id: 'vila_vitoria', name: 'Vila Vitória', latitude: -2.4700, longitude: -44.2500 },
+  { id: 'vila_vitoria', name: 'Vila Vitória', latitude: -2.6070, longitude: -44.2049 },
 
-  { id: 'vinhais', name: 'Vinhais', latitude: -2.5164, longitude: -44.2741 }
+  { id: 'vinhais', name: 'Vinhais', latitude: -2.5139, longitude: -44.2554 }
 ];
