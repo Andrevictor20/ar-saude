8e20f1045e8c9fb56e12ae8ca3f5d66c2fccb7f8
André Victor Macedo Pereira <victorandre280@gmail.com>
2026-06-08 11:16:47 -0300

    Configure HTTPS agent for HttpModule
    Added HTTPS agent configuration to HttpModule.

--- motor-alertas/src/interscity/interscity.module.ts
+++ motor-alertas/src/interscity/interscity.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { InterscityReaderService } from './interscity-reader.service';
+import * as https from 'https'; // <-- 1. Importação necessária
 
 @Module({
   imports: [
     HttpModule.register({
       timeout: 15_000,
       maxRedirects: 3,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
     }),
   ],
   providers: [InterscityReaderService],
